b3ab7fb41a224bde7e58058c3a5f43ffe5758132
Haojie Zhu <zhuhj@umich.edu>
2025-01-22 15:49:23 -0500

    minor change for nuboard replay

--- src/safedriver_nuplan/planner/models/safedriver.py
+++ src/safedriver_nuplan/planner/models/safedriver.py
@@ -1,7 +1,11 @@
 import os
 import torch
 
-safedriver_path = os.environ["SAFEDRIVER_ACTIONHEAD_PATH"]
-print(safedriver_path)
-safedriver_module = torch.jit.load(safedriver_path)
-safedriver_module.eval()
+try:
+    safedriver_path = os.environ["SAFEDRIVER_ACTIONHEAD_PATH"]
+    print(safedriver_path)
+    safedriver_module = torch.jit.load(safedriver_path)
+    safedriver_module.eval()
+except Exception as e:
+    print("Warning: Could not load SafeDriver model")
+    safedriver_module = None
